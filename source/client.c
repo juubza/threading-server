acdd1015f113f7ab3002c9a6017b4069ae992bab
FelipeGVenancio <141284277+FelipeGVenancio@users.noreply.github.com>
2024-09-17 20:53:23 +0000

    modified:   source/client.c

--- source/client.c
+++ source/client.c
@@ -62,7 +62,7 @@ void connect_and_request(const char* path, char request_type) {
 
 int main() {
     char type;
-    printf("Opção e : exit \n");
+    printf("Opção (e) : exit \n");
     while (type != 'e')
     {
     printf("Digite o tipo de dado a ser solicitado: (s/i)\n");
